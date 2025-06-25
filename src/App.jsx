447365b03e233634993f2c85379b62afc92794f0
Amber Chapman <alichappy@gmail.com>
2025-06-26 01:15:02 +1000

    tailwind vite fix

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className="min-h-screen flex items-center justify-center bg-green-200">
+      <h1 className="text-3xl font-bold text-gray-800">Tailwind is working!</h1>
+    </div>
+    </>
+  )
+}
+
+export default App
