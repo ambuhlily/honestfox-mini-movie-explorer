571ab98875382451870a9cc4ff5488b449809d80
Amber Chapman <alichappy@gmail.com>
2025-06-26 15:49:30 +1000

    movie details page basic setup

--- README.md
+++ README.md
@@ -6,3 +6,13 @@ Currently, two official plugins are available:
 
 - [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
 - [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
+
+issues:
+- vite version 7 dependencies issue with tailwind
+
+how i approached
+- planned out scope
+- spun up basic react + vite + tailwind app which took quite a while because of dependencies issues between new vite version and tailwind
+- codebase structure setup
+- created all my pages and set up routing using react router
+- home page fetch and display
--- src/pages/MovieDetails.jsx
+++ src/pages/MovieDetails.jsx
@@ -1,13 +1,36 @@
 import { Link } from 'react-router-dom';
+import { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+const omdbapiUrl = "http://www.omdbapi.com/";
+const apiKey = "adf1f2d7";
 
 const MovieDetails = () => {
+  const { id } = useParams();
+  const [movie, setMovie] = useState(null);
+
+  useEffect(() => {
+    fetch(`${omdbapiUrl}?i=${id}&apikey=${apiKey}`)
+      .then((res) => res.json())
+      .then((data) => setMovie(data));
+  }, [id]);
+
+  if (!movie) return <p>Loading...</p>;
 
   return (
     <div className="p-4">
       <h1>Movie Details</h1>
 
+      <div className="p-4 text-gray-800 bg-gray-100 rounded-md shadow-md">
+      <h1 className="text-3xl font-bold mb-4">{movie.Title}</h1>
+      <img src={movie.Poster} alt={movie.Title} className="w-64 mb-4" />
+      <p><strong>Year:</strong> {movie.Year}</p>
+      <p>Genre: {movie.Genre}</p>
+      <p>Plot: {movie.Plot}</p>
+      {/* favourite button */}
+    </div>
+
       <Link to="/" className="text-blue-500 hover:underline">
-        Go back to Home
+        Back to results
       </Link>
     </div>
   );
